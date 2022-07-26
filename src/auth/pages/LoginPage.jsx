69e6db51a573baceacf31d678fd04ac238932909
Nahum Casco <nahumcasco.v@gmail.com>
2022-07-25 21:40:01 -0500

    error alerts and state behavior

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,30 +1,39 @@
+import { useMemo, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link as RouterLink } from 'react-router-dom'
-import { checkAuthentication, startGoogleSignIn } from '../../store/auth/thunks'
+import { checkAuthentication, startGoogleSignIn, startLoginWithEmailPassword } from '../../store/auth/thunks'
 import { Google } from '@mui/icons-material'
-import { Button, Grid, Link, TextField, Typography } from '@mui/material'
+import { Button, Grid, Link, TextField, Typography, Alert } from '@mui/material'
 import { AuthLayout } from '../layouts/AuthLayout'
 import { useForm } from '../../hooks'
-import { useMemo } from 'react'
+
+
+const formValidations = {
+  email: [(value) => value.length > 1, 'ingrese el correo'],
+  password: [(value) => value.length > 1, 'ingrese la contraseña']
+}
 
 export const LoginPage = () => {
-  const { status } = useSelector(state => state.auth)
-  const { email, password, onInputChange, onResetForm } = useForm({email: '', password: ''})
+  const [formSubmitted, setFormSubmitted] = useState(false)
+  const { status, errorMessage } = useSelector(state => state.auth)
+  const { email, password, formState, onInputChange, onResetForm, emailValid, passwordValid, isFormValid } = useForm({ email: '', password: '' }, formValidations)
   const dispatch = useDispatch()
 
   const isAuthenticated = useMemo(() => status === 'checking', [status])
-  
+
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log({email, password})
-    dispatch(checkAuthentication()) 
+    setFormSubmitted(true)
+    if(!isFormValid) return;
+    dispatch(startLoginWithEmailPassword(formState))
+    onResetForm()
+    setFormSubmitted(false)
   }
 
   const onGoogleSignIn = () => {
-    console.log('google signin')
     dispatch(startGoogleSignIn())
   }
-  
+
   return (
     <AuthLayout title="Login">
       <form onSubmit={handleSubmit}>
@@ -38,6 +47,8 @@ export const LoginPage = () => {
               fullWidth
               value={email}
               onChange={onInputChange}
+              error={!!emailValid && formSubmitted}
+              helperText={formSubmitted && emailValid}
             />
           </Grid>
 
@@ -50,24 +61,30 @@ export const LoginPage = () => {
               fullWidth
               value={password}
               onChange={onInputChange}
+              error={!!passwordValid && formSubmitted}
+              helperText={formSubmitted && passwordValid}
             />
           </Grid>
 
           <Grid container spacing={2} sx={{ mb: 2, mt: 2 }}>
+            <Grid item xs={12} display={!!errorMessage ? '' : 'none'}>
+              <Alert severity="error">{errorMessage}</Alert>
+            </Grid>
+
             <Grid item xs={12} sm={6}>
-              <Button 
-                type="submit" 
-                variant='contained' 
+              <Button
+                type="submit"
+                variant='contained'
                 fullWidth
                 disabled={isAuthenticated}
               >
-                  Login
+                Login
               </Button>
             </Grid>
             <Grid item xs={12} sm={6}>
-              <Button 
-                onClick={onGoogleSignIn} 
-                variant='contained' 
+              <Button
+                onClick={onGoogleSignIn}
+                variant='contained'
                 fullWidth
                 disabled={isAuthenticated}
               >
