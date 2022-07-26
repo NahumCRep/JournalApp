3577b5246ca70fbaeae9aa39746accc97b6383e4
Nahum Casco <nahumcasco.v@gmail.com>
2022-07-25 21:34:34 -0500

    logout

--- src/journal/components/Navbar.jsx
+++ src/journal/components/Navbar.jsx
@@ -1,34 +1,42 @@
 import { MenuOutlined, LogoutOutlined } from '@mui/icons-material'
 import { AppBar, Grid, IconButton, Toolbar, Typography } from '@mui/material'
 import PropTypes from 'prop-types'
+import { useDispatch } from 'react-redux'
+import { startLogout } from '../../store/auth/thunks'
 
-export const Navbar = ({drawerWidth}) => {
-  return (
-    <AppBar 
-        position='fixed' 
-        sx={{
-            width:{ sm: `calc(100% - ${drawerWidth}px)`},
-            ml: { sm: `${drawerWidth}px`}
-        }}
-    >
-        <Toolbar>
-            <IconButton
-                color='inherit'
-                edge="start"
-                sx={{ mr: 2, display:{ sm: 'none'} }}
-            >
-                <MenuOutlined />
-            </IconButton>
+export const Navbar = ({ drawerWidth }) => {
+    const dispatch = useDispatch()
 
-            <Grid container direction="row" justifyContent="space-between" alignItems="center" >
-                <Typography variant='h6' noWrap component="div">Journal App</Typography>
-                <IconButton color='error'>
-                    <LogoutOutlined />
+    const onLogout = () => {
+        dispatch(startLogout())    
+    }
+
+    return (
+        <AppBar
+            position='fixed'
+            sx={{
+                width: { sm: `calc(100% - ${drawerWidth}px)` },
+                ml: { sm: `${drawerWidth}px` }
+            }}
+        >
+            <Toolbar>
+                <IconButton
+                    color='inherit'
+                    edge="start"
+                    sx={{ mr: 2, display: { sm: 'none' } }}
+                >
+                    <MenuOutlined />
                 </IconButton>
-            </Grid>
-        </Toolbar>
-    </AppBar>
-  )
+
+                <Grid container direction="row" justifyContent="space-between" alignItems="center" >
+                    <Typography variant='h6' noWrap component="div">Journal App</Typography>
+                    <IconButton color='error' onClick={onLogout}>
+                        <LogoutOutlined />
+                    </IconButton>
+                </Grid>
+            </Toolbar>
+        </AppBar>
+    )
 }
 
 Navbar.propTypes = {
