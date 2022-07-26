a3762c45b4d9eef6087aaed65c5f6b514c422edd
Nahum Casco <nahumcasco.v@gmail.com>
2022-07-25 21:36:11 -0500

    auth slice, thunks

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,4 +1,4 @@
-import { registerWithEmailPassword, signInWithGoogle } from "../../firebase/providers"
+import { loginWithEmailPassword, logoutFirebase, registerWithEmailPassword, signInWithGoogle } from "../../firebase/providers"
 import { checkingCredentials, logout, login } from "./authSlice"
 
 export const checkAuthentication = () => {
@@ -25,5 +25,23 @@ export const startRegisterUserWithEmailPassword = ({displayName, email, password
         if( !ok ) return dispatch(logout(errorMessage))
 
         dispatch(login({uid, displayName, email, photoURL}))
+    }
+}
+
+export const startLoginWithEmailPassword = ({email, password}) => {
+    return async (dispatch) => {
+        dispatch(checkingCredentials())
+        const {ok, uid, displayName, photoURL, errorMessage} = await loginWithEmailPassword({email, password})
+        
+        if (!ok) return dispatch(logout(errorMessage))
+
+        dispatch(login({uid, displayName, email, photoURL}))
+    }
+}
+
+export const startLogout = () => {
+    return async (dispatch) => {
+        await logoutFirebase()
+        dispatch(logout())
     }
 }
