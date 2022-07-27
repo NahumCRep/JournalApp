d9ac9d4924d577c3a29e515349f6300c3a322e7d
Nahum Casco <nahumcasco.v@gmail.com>
2022-07-26 21:41:46 -0500

    sidebar item component

---
+++ src/journal/components/SideBarItem.jsx
@@ -0,0 +1,33 @@
+import { useMemo } from 'react'
+import { useDispatch } from 'react-redux'
+import { TurnedInNot } from '@mui/icons-material'
+import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
+import { setActiveNote } from '../../store/journal/journalSlice'
+
+export const SideBarItem = ({id, title, body, date, imageUrls = []}) => {
+    const dispatch = useDispatch()
+
+    const newTitle = useMemo(() => {
+        return title.length > 17 
+            ? title.substring(0, 17) + '...'
+            : title
+    }, [title])
+
+    const onSetActiveNote = () => {
+        dispatch(setActiveNote({id, title, body, date, imageUrls}))
+    }
+
+    return (
+        <ListItem disablePadding>
+            <ListItemButton onClick={onSetActiveNote}>
+                <ListItemIcon>
+                    <TurnedInNot />
+                </ListItemIcon>
+                <Grid container>
+                    <ListItemText primary={newTitle} />
+                    <ListItemText secondary={body} />
+                </Grid>
+            </ListItemButton>
+        </ListItem>
+    )
+}
