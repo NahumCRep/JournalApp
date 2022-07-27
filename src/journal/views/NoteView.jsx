3dcee022f8476cc622988e6a3066c5083ff7a3de
Nahum Casco <nahumcasco.v@gmail.com>
2022-07-26 21:43:03 -0500

    form values and save note

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,43 +1,69 @@
-import { Button, Grid, TextField, Typography } from '@mui/material'
-import { SaveOutlined } from '@mui/icons-material'
-import React from 'react'
-import { ImageGallery } from '../components'
+import { useSelector, useDispatch } from 'react-redux';
+import { SaveOutlined } from '@mui/icons-material';
+import { Button, Grid, TextField, Typography } from '@mui/material';
+import { ImageGallery } from '../components';
+import { useForm } from '../../hooks';
+import moment from 'moment';
+import { useEffect } from 'react';
+import { setActiveNote } from '../../store/journal/journalSlice';
+import { startSaveNote } from '../../store/journal/thunks';
 
 export const NoteView = () => {
-  return (
-    <Grid container direction="row" justifyContent="space-between" alignItems="center" sx={{mb: 1}}>
-        <Grid item>
-            <Typography fontSize={39} fontWeight="light">28 de Agosto</Typography>
-        </Grid>
-        <Grid item>
-            <Button color="primary" sx={{padding: 2}}>
-                <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
-                Guardar
-            </Button>
-        </Grid>
+    const dispatch = useDispatch();
+    const {active: note} = useSelector(state => state.journal);
+    const {title, body, date, formState, onInputChange} = useForm(note);
 
-        <Grid container>
-            <TextField 
-                type="text"
-                variant="filled"
-                fullWidth
-                placeholder='ingrese un titulo'
-                label="Titulo"
-                sx={{boder: 'none', mb: 1}}
-            />
+    useEffect(() => {
+        dispatch(setActiveNote(formState))
+    }, [formState])
 
-            <TextField 
-                type="text"
-                variant="filled"
-                fullWidth
-                multiline
-                placeholder='Que sucedio hoy?'
-                minRows={5}
-            />
+    const onSaveNote = () => {
+        dispatch(startSaveNote())
+    }
+
+    return (
+        <Grid container direction="row" justifyContent="space-between" alignItems="center" sx={{ mb: 1 }}>
+            <Grid item>
+                <Typography fontSize={39} fontWeight="light">{moment(date).format('LL')}</Typography>
+            </Grid>
+            <Grid item>
+                <Button 
+                    color="primary" 
+                    sx={{ padding: 2 }}
+                    onClick={onSaveNote}
+                >
+                    <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
+                    Guardar
+                </Button>
+            </Grid>
+
+            <Grid container>
+                <TextField
+                    type="text"
+                    variant="filled"
+                    fullWidth
+                    placeholder='ingrese un titulo'
+                    label="Titulo"
+                    sx={{ boder: 'none', mb: 1 }}
+                    name="title"
+                    value={title}
+                    onChange={onInputChange}
+                />
+
+                <TextField
+                    type="text"
+                    variant="filled"
+                    fullWidth
+                    multiline
+                    placeholder='Que sucedio hoy?'
+                    minRows={5}
+                    name="body"
+                    value={body}
+                    onChange={onInputChange}
+                />
+            </Grid>
+
+            <ImageGallery />
         </Grid>
-
-        <ImageGallery />
-    </Grid>
-  )
+    )
 }
- 
