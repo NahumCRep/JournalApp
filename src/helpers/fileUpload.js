c3a9d27bab53550e2b4aae556c385cbb678f82f5
Nahum Casco <nahumcasco.v@gmail.com>
2022-07-27 21:43:16 -0500

    upload files

---
+++ src/helpers/fileUpload.js
@@ -0,0 +1,22 @@
+
+export const fileUpload = async (file) => {
+    if(!file) throw new Error('no hay ningun archivo a subir');
+
+    const cloudURL = 'https://api.cloudinary.com/v1_1/nahumcasco-v/upload';
+    const formData = new FormData();
+    formData.append('upload_preset', 'react-journal'); 
+    formData.append('file', file); 
+
+    try {
+        const resp = await fetch(cloudURL, {
+            method: 'POST',
+            body: formData,
+        })
+
+        if(!resp.ok) throw new Error('no se pudo subir la imagen');
+        const cloudResp = await resp.json();
+        return cloudResp.secure_url;
+    } catch (error) {
+        throw new Error(error.message)
+    }
+}
